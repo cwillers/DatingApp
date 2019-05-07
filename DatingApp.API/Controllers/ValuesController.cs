6ef4afab2146582f194528e2b17d947149ce66ab
Clifford Willers <cwillers@iqbusiness.net>
2019-05-07 11:38:54 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         // public ActionResult<IEnumerable<string>> Get()
         // {
